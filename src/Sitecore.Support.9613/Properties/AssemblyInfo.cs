70ba7de751b4cde7b2baafc10a9c94fbbf53683d
DK\IEKA <ieka@sitecore.net>
2017-12-22 13:53:40 +0200

    Fix error when "List Facet" uses "Single-Line Text" filed that has"1234" (number) value

---
+++ src/Sitecore.Support.9613/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.9613")]
+[assembly: AssemblyProduct("Sitecore.Support.9613")]
+[assembly: ComVisible(false)]
