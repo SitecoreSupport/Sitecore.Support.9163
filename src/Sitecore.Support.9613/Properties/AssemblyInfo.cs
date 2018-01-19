20427724afe4da4759ba74bfcb37581bbe62d99e
ieka <ieka@sitecore.net>
2018-01-19 14:14:59 +0200

    Rename assembly name

--- src/Sitecore.Support.9613/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.9613/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.9613")]
-[assembly: AssemblyProduct("Sitecore.Support.9613")]
+[assembly: AssemblyTitle("Sitecore.Support.9163")]
+[assembly: AssemblyProduct("Sitecore.Support.9163")]
 [assembly: ComVisible(false)]
